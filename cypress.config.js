e3f10392340791918e66d6cb78340e8048234804
tanhab20 <tanhab20@htl-kaindorf.at>
2024-12-16 18:30:03 +0100

    specpattern

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://web:3000',
-    specPattern: 'cypress/e2e/**/*.cy.ts',
+   // specPattern: 'cypress/e2e/**/*.cy.ts',
+    specPattern: 'cypress/e2e/performance.cy.ts',
     supportFile: false,
   },
 });
