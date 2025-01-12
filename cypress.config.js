e79c3a4542fd96d09dea2cf6fe671ff709233e8a
tanhab20 <tanhab20@htl-kaindorf.at>
2025-01-12 21:59:10 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://web:3000',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.ts',
    // specPattern: 'cypress/e2e/performance.cy.ts',
     supportFile: false,
