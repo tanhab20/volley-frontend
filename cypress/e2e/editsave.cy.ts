d16852107c3c201e19509ecfe93d8f5be3edf84c
tanhab20 <tanhab20@htl-kaindorf.at>
2025-01-18 22:29:06 +0100

    Update editsave.cy.ts

--- cypress/e2e/editsave.cy.ts
+++ cypress/e2e/editsave.cy.ts
@@ -50,10 +50,9 @@ describe('Edit Tournament', () => {
         // Warten, bis das Element mit den aktualisierten Daten geladen wird
         cy.get('.tournament-list-item').first().within(() => {
             // Warte explizit darauf, dass das Element geladen wird
-            cy.get('#datum').should('exist');  // Sicherstellen, dass das Datum vorhanden ist
 
             // Verwende 'contains' anstatt 'should' um nach dem Datum zu suchen
-            cy.get('#datum').contains('2024-07-20').should('exist');  // Überprüfen, ob das Datum korrekt ist
+            cy.get('#datum').contains('20.7.2024').should('exist');  // Überprüfen, ob das Datum korrekt ist
 
             // Überprüfen, ob der Veranstaltungsort korrekt ist
             cy.get('#ort').contains(updatedLocation).should('exist');
