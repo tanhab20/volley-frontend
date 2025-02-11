7eaea324aca4c0ae7c35953697863fd9495ab399
tanhab20 <tanhab20@htl-kaindorf.at>
2025-02-11 12:21:34 +0100

    all test fixed

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://kavolley.uber.space/tournaments',
-   //specPattern: 'cypress/e2e/**/*.cy.ts',
-    specPattern: 'cypress/e2e/search.cy.ts',
+   specPattern: 'cypress/e2e/**/*.cy.ts',
+    //specPattern: 'cypress/e2e/search.cy.ts',
     supportFile: false,
   },
 });
