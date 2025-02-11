350b536481b207b315db6b27a97bebb7f033eaef
tanhab20 <tanhab20@htl-kaindorf.at>
2025-02-11 14:56:38 +0100

    all test fixed

--- cypress/e2e/createTournament.cy.ts
+++ cypress/e2e/createTournament.cy.ts
@@ -1,8 +1,7 @@
 import * as cypress from "cypress";
 
-
 describe('Tournament Form', () => {
-    it('should fill out the form and submit successfully', () => {
+    it('should fill out the form and verify tournament count increases', () => {
         cy.visit('https://kavolley.uber.space/');
 
         // Login
@@ -13,34 +12,45 @@ describe('Tournament Form', () => {
         // Stelle sicher, dass die richtige Seite geladen wurde
         cy.visit('https://kavolley.uber.space/');
 
+        cy.wait(1000);
 
-        // Navigiere zur Formularseite
-        cy.get('.navbar-link').contains('Form').click();
-        cy.url().should('include', '/form');
+        // Anzahl der Turniere speichern
+        cy.get('.tournament-list-item').its('length').then((initialCount) => {
 
-        const tournamentData = {
-            name: 'Test Turnier',
-            date: '2024-10-05',
-            venue: 'Sporthalle 1',
-            duration: '2 Stunden',
-            description: 'Ein spannendes Test-Turnier'
-        };
+            // Navigiere zur Formularseite
+            cy.get('.navbar-link').contains('Form').click();
+            cy.url().should('include', '/form');
 
-        // Fülle das Formular aus
-        cy.get('input[id="name"]').type(tournamentData.name);
-        cy.get('input[id="date"]').type(tournamentData.date);
-        cy.get('input[id="venue"]').type(tournamentData.venue);
-        cy.get('input[id="duration"]').type(tournamentData.duration);
-        cy.get('textarea[id="description"]').type(tournamentData.description);
+            const tournamentData = {
+                name: 'Test Turnier',
+                date: '2024-10-05',
+                venue: 'Sporthalle 1',
+                duration: '2 Stunden',
+                description: 'Ein spannendes Test-Turnier'
+            };
 
-        // Stelle sicher, dass der Button sichtbar ist, und klicke
-        cy.get('button[id="submitButton"]').should('be.visible').click();
+            // Fülle das Formular aus
+            cy.get('input[id="name"]').type(tournamentData.name);
+            cy.get('input[id="date"]').type(tournamentData.date);
+            cy.get('input[id="venue"]').type(tournamentData.venue);
+            cy.get('input[id="duration"]').type(tournamentData.duration);
+            cy.get('textarea[id="description"]').type(tournamentData.description);
 
-        // Stelle sicher, dass die Übersicht geladen wurde
-        cy.url().should('include', '/overview');
+            // Stelle sicher, dass der Button sichtbar ist, und klicke
+            cy.get('button[id="submitButton"]').should('be.visible').click();
 
-        // Stelle sicher, dass der Button sichtbar ist, und klicke
-        cy.get('button[id="submitButton"]').should('be.visible').click();
+            // Überprüfe, dass die Seite geladen wurde
+            cy.url().should('include', '/overview');
 
+            // Gehe zurück zur Turnierliste
+            cy.get('button[id="submitButton"]').should('be.visible').click();
+            cy.url().should('include', '/tournaments');
+
+            // Warte auf die Aktualisierung der Liste
+            cy.wait(1000);
+
+            // Überprüfe, ob genau 1 neues Turnier hinzugekommen ist
+            cy.get('.tournament-list-item').should('have.length', initialCount + 1);
+        });
     });
 });
